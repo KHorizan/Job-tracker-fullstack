afbcf6f73aa9459bffdc71407d611812f6fdfd09
KHorizan <kashishrauthan43@gmail.com>
2026-01-01 22:39:11 +0530

    Setup backend and Mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Backend is working")
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() =>{
+    console.log(`sever running on port  ${PORT}`)
+});
+
