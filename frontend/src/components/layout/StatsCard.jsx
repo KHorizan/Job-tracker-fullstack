be522cf4440817f3d0de2cb6d7f685efbb87866a
KHorizan <kashishrauthan43@gmail.com>
2026-01-06 17:31:17 +0530

    Added action component and changed UI

--- frontend/src/components/layout/StatsCard.jsx
+++ frontend/src/components/layout/StatsCard.jsx
@@ -2,10 +2,10 @@ import "../../Styles/StatsCard.css";
 
 function StatsCard({title,value,subtitle,className=""}){
   return(
-    <div className={`stats-card ${className}`}>
+    <div className={`stats-card`}>
       <h2 className="stats-title">{title}</h2>
       <p className="stats-value">{value}</p>
-      {subtitle && <p className="stats-subtitle">{subtitle}</p>}
+      {subtitle && <p className="stats-subtitle" title={subtitle}>{subtitle}</p>}
     </div>
 
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../components/layout/Sidebar";
 import Topbar from "../components/layout/Topbar";
 import StatsCard from"../components/layout/StatsCard";
 import Charts from "../components/layout/InterviewChart";
+import ActionCard from"../components/layout/ActionCard";
+
 import "../Styles/Dashboard.css";
 
 function Dashboard(){
@@ -13,11 +15,41 @@ function Dashboard(){
             <Topbar title="Dashboard" username="Kashish" /> 
 
             <div className="stats-container">
-                <StatsCard title="Attended" value={12} className="purple" />
-                <StatsCard title="Pending" value={9} className="green" />
-                <StatsCard title="Rejected" value={3} className="red" />
+                <StatsCard title="Total Jobs" value={24} subtitle="Total jobs you have applied to"/>
+                <StatsCard title="Attended" value={12} subtitle="Total interviews you have attende" />
+                <StatsCard title="Pending" value={9}  subtitle="Applications that are still pending"/>
+                <StatsCard title="Rejected" value={3} subtitle="Applications that were rejected" />
             </div>
 
+
+        <div className="actionCard-layout">
+        <ActionCard 
+        title="Add a Job"
+        description ="Quickly add a new job you’re interested in tracking."
+        button ="Add Job"
+        to="/add-job"
+        />
+
+        <ActionCard 
+        title="View Jobs"
+        description ="Browse all available jobs and find the ones that match your profile"
+        button ="View Jobs"
+        to="/jobs"
+        />
+        <ActionCard 
+        title="Update profile"
+        description ="Edit your personal information, contact details, and account settings."
+        button ="Edit Profile"
+        to="/profile"
+        />
+        <ActionCard 
+        title="Seetings"
+        description ="Customize your account preferences, notifications, and other options."
+        button ="Update Setting"
+        to="/settings"
+        />
+        </div>
+        
             <Charts />
 
             </div>  
