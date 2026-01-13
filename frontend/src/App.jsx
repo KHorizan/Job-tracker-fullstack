363b8283a7cc22e69200f31375120c114c808a83
KHorizan <kashishrauthan43@gmail.com>
2026-01-13 17:08:40 +0530

    Updated login styling,Register, and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import Dashboard from "./pages/Dashboard";
+import{Routes, Route}  from "react-router-dom";
+import Login from  "./pages/auth/Login.jsx";
+import Register from  "./pages/auth/Register.jsx";
 
+import "./Styles/App.css";
 
 function App() {
-  return (
-    <div className ="App-layout">
-      <Dashboard />
-    </div>
-  );
+   return (
+    <Routes>
+      <Route path ="/login" element={<Login />} />
+      <Route path ="/register" element={<Register />} />
+
+    </Routes>
+   );
 }
 
 export default App;
