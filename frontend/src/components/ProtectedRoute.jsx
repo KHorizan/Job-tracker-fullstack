5f8f71003a23c7816a0a140d9bcdb16da38293ec
KHorizan <kashishrauthan43@gmail.com>
2026-01-14 18:41:29 +0530

    Created ProtectedRoute to restrict access based on role

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import {Navigate} from "react-router-dom";
+import{useAuth} from "../context/AuthContext";
+
+function ProtectedRoute({children,allowedRoles}) {
+    const{user} = useAuth();
+
+    if(!user) return <Navigate to="/login"  state={{ from: location }}  />;
+    if(allowedRoles && !allowedRoles.includes(user.role)) return <Navigate to ="/dashboard" />;
+
+    return children;
+}
+export default ProtectedRoute;
