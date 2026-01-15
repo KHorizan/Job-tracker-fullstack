e1b97c8ff2d30e4e8e2b794c51c74b2a7b01fe08
KHorizan <kashishrauthan43@gmail.com>
2026-01-15 18:05:03 +0530

    Sidebar and Topbar components

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -1,15 +1,18 @@
 import "../../Styles/Topbar.css";
 
-function Topbar({title,username}){
-    return(
-        <div className="topbar">
-           <h2 className="page-title">{title}</h2>
-
-           <div className="user-info">
-            <span className="username">{username}</span>
-           </div>
-        </div>
-    );
+function Topbar({ title, username, toggleSidebar }) {
+  return (
+    <div className="topbar">
+      <div className="topbar-left">
+        <button className="menu-btn" onClick={toggleSidebar}>☰</button>
+        <span className="page-title">{title}</span>
+      </div>
+      <div className="user-info">
+        <span className="username">{username}</span>
+      </div>
+    </div>
+  );
 }
+
 
 export default Topbar;
--- frontend/src/components/layout/sidebar.jsx
+++ frontend/src/components/layout/sidebar.jsx
@@ -1,18 +1,29 @@
 import "../../Styles/Sidebar.css";
+import sidebarMenu from"../../config/menu";
+import{NavLink} from"react-router-dom";
 
 
-function Sidebar(){
+function Sidebar({role,isMobile,isOpen,onClose}){
+  const items = sidebarMenu[role]||[];
     return (
-        <div className="sidebar">
+        <div className={`sidebar ${isMobile? "mobile":"desktop"}  ${ isOpen ?"open":"closed"}`}>
             <h2 className="logo">Job-Tracker</h2>
 
           <ul className="nav-links">
-            <li className="active">Dashboard</li>
-            <li>Jobs</li>
-            <li>Stats</li>
-            <li>Profile</li>
+            {items.map((item)=>(
+              <li key={item.link}>
+            <NavLink  to={item.link} 
+            className={({isActive}) => (isActive?"active":"")}
+            onClick={isMobile?onClose:undefined}>
+              
+              {item.name}
+            </NavLink>
+              </li>
+            ))}
+           
           </ul>
         </div>
+  
     );
 }
 export default Sidebar;
