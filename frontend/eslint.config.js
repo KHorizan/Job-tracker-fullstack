7deeec6285d4d7e9f59dbd66d3fde5aa6ce584c2
KHorizan <kashishrauthan43@gmail.com>
2026-01-15 18:13:55 +0530

    statscard styling

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -25,5 +25,12 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'], // <--- add this
+        },
+      },
+    },
   },
-])
+]);
