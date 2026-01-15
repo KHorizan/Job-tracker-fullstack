1d0feb3ce0dee1e724ee00a31eb3b466e19686f0
KHorizan <kashishrauthan43@gmail.com>
2026-01-15 18:09:57 +0530

    Modified app.jsx and added interceptors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import{Routes, Route}  from "react-router-dom";
 import Login from  "./pages/auth/Login.jsx";
 import Register from  "./pages/auth/Register.jsx";
-
+import Dashboard from "./pages/jobseeker/dashboard.jsx";
 import "./Styles/App.css";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AppLayout from "./components/layout/AppLayout.jsx";
 
 function App() {
    return (
@@ -10,6 +12,17 @@ function App() {
       <Route path ="/login" element={<Login />} />
       <Route path ="/register" element={<Register />} />
 
+
+      <Route 
+        element={
+       <ProtectedRoute allowedRoles={["jobseeker"]}>
+        <AppLayout />
+      </ProtectedRoute>
+      }
+      >
+        <Route path ="/dashboard" element={<Dashboard />} />
+    </Route >
+      
     </Routes>
    );
 }
---
+++ frontend/src/config/menu.js
@@ -0,0 +1,16 @@
+ const sidebarMenu = {
+jobseeker:[
+    {name:"Dashboard",link:"/dashboard"},
+    {name:"My Applications",link:"/applications"},
+    {name:"Search",link:"/search"}
+   
+    
+],
+admin:[
+    {name:"RoleManagement" ,link:"/role"},
+    {name:"Dashboard",link:"/dashbaord"},
+    {name:"Profile",link:"/profile"}
+],
+};
+
+export default sidebarMenu;
