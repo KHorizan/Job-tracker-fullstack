99c8cefb20ee3971b216a999827ffe64ed001101
KHorizan <kashishrauthan43@gmail.com>
2026-02-04 18:59:27 +0530

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx';
+import { AppliedProvider } from './context/AppliedContext.jsx';
+
+const token = localStorage.getItem("token");
+
+if (token) {
+  try {
+    const decoded = jwtDecode(token);
+    const now = Date.now() / 1000;
+
+    if (decoded.exp < now) {
+      localStorage.removeItem("token");
+    }
+  } catch {
+    localStorage.removeItem("token");
+  }
+}
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
    <AuthProvider>
-    <App />
+    <AppliedProvider>
+       <App />
+    </AppliedProvider>
     </AuthProvider>
   </BrowserRouter>
 );
